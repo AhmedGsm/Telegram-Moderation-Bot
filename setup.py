f7339a5d1396a559f508d19a543f08843a6f4f20
Ahmed KHABER <ahmed.gsm1983@gmail.com>
2025-09-20 10:11:48 +0100

    Add navigation menu

--- setup.py
+++ setup.py
@@ -215,5 +215,5 @@ def bot_page():
     return render_template('bot.html')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', False))
+    port = int(os.environ.get("PORT", 4320))
+    app.run(host='0.0.0.0', port=port, debug=os.environ.get('DEBUG', True))
