e7c0adf7a56aba8a2e28a6c2f63dff1303464fcd
Ahmed KHABER <ahmed.gsm1983@gmail.com>
2025-11-21 21:30:09 +0100

    Create database class

---
+++ userdb.py
@@ -0,0 +1,80 @@
+import sqlite3
+from datetime import datetime
+
+class UserDB:
+    def __init__(self, path="database.db"):
+        self.conn = sqlite3.connect(path, check_same_thread=False)
+        self.create_table()
+
+    def create_table(self):
+        query = """
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY,
+            username TEXT,
+            first_name TEXT,
+            last_name TEXT,
+            phone TEXT,
+            language_code TEXT,
+            is_bot BOOLEAN,
+            join_date TIMESTAMP,
+            last_seen TIMESTAMP,
+
+            approved_posts INTEGER DEFAULT 0,
+            rejected_posts INTEGER DEFAULT 0,
+            warn_count INTEGER DEFAULT 0,
+            kick_count INTEGER DEFAULT 0,
+            mute_count INTEGER DEFAULT 0,
+            ban_count INTEGER DEFAULT 0,
+
+            actual_state TEXT DEFAULT 'active'
+        );
+        """
+        self.conn.execute(query)
+        self.conn.commit()
+
+    def ensure_user(self, user):
+        query = """
+        INSERT OR IGNORE INTO users (id, username, first_name, last_name, phone,
+                                     language_code, is_bot, join_date, last_seen)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """
+
+        params = (
+            user.id,
+            user.username,
+            user.first_name,
+            user.last_name,
+            user.phone,
+            user.lang_code if hasattr(user, "lang_code") else None,
+            user.bot if hasattr(user, "bot") else False,
+            datetime.now(),
+            datetime.now(),
+        )
+
+        self.conn.execute(query, params)
+        self.conn.commit()
+
+    def increment(self, user_id, field):
+        query = f"""
+            UPDATE users
+            SET {field} = {field} + 1,
+                last_seen = ?
+            WHERE id = ?
+        """
+        self.conn.execute(query, (datetime.now(), user_id))
+        self.conn.commit()
+
+    def set_state(self, user_id, state):
+        query = """
+            UPDATE users
+            SET actual_state = ?, last_seen = ?
+            WHERE id = ?
+        """
+        self.conn.execute(query, (state, datetime.now(), user_id))
+        self.conn.commit()
+
+    def get_user(self, user_id):
+        q = "SELECT * FROM users WHERE id = ?"
+        cur = self.conn.execute(q, (user_id,))
+        return cur.fetchone()
+
